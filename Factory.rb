7775621a8a3ba229697b02282c98554e99a520b8
Sirchik <sirchik@gmail.com>
2015-10-29 01:03:06 +0200

    Almost done

--- Factory.rb
+++ Factory.rb
@@ -1,7 +1,73 @@
 class Factory
 
-  def initialize *args, &block
-    Class.new
+  def self.new *args, &block
+    if args[0].is_a?(String)
+      class_name = args.shift
+      p class_name
+    end
+    my_class = Class.new do 
+      @@members = args
+
+      define_method :initialize do |*arr|
+        @vars = @@members.zip(arr).to_h
+      end
+
+      @@members.each do |v|
+      
+        define_method v do
+          @vars[v]
+        end
+
+        define_method "#{v}=" do |new_value|
+          @vars[v] = new_value
+        end
+
+      end
+      
+      define_method "#{:[]}=" do |index, new_value|
+        @vars[get_elem_sym(index)] = new_value
+      end
+
+      define_method :[] do |index|
+        @vars[get_elem_sym(index)]
+      end
+
+      define_method :inspect do
+        vars_string = ''
+        @vars.each { |key, value| vars_string += ', ' unless vars_string.empty?; vars_string += "#{key}=#{value}"}
+        '#<factory ' + my_class.inspect + ' ' + vars_string + '>'
+      end
+
+      define_method :members do
+        my_class.members
+      end
+
+      define_singleton_method :members do
+        @@members
+      end
+
+      private
+
+        def get_elem_sym index
+          if index.is_a?(Numeric)
+            @vars.keys[index.to_i]
+          elsif index.is_a?(Symbol)
+            index
+          elsif index.is_a?(String)
+            index.to_sym
+          else
+            fail TypeError, "No implicit conversion of #{index.class} into Integer"
+          end
+        end
+
+      class_eval &block if block_given?
+
+    end
+
+    const_set(class_name, my_class) if class_name
+
+    my_class
+
   end
 
 end
--- Factory_spec.rb
+++ Factory_spec.rb
@@ -2,13 +2,77 @@ require_relative 'Factory'
 
 describe Factory do
 
-  it 'Factory new' do
+  CustomerS = Struct.new(:name, :address, :zip)
+  CustomerF = Factory.new(:name, :address, :zip)
+  joeS = CustomerS.new('Joe Smith', '123 Maple, Anytown NC', 12345)
+  joeF = CustomerF.new('Joe Smith', '123 Maple, Anytown NC', 12345)
+  Factory.new("CustomerString", :name, :address, :zip)
 
-    CustomerS = Struct.new(:name, :address, :zip)
-    p CustomerS
-    CustomerF = Factory.new(:name, :address, :zip)
-    p CustomerF
+  it 'new CustomerString' do 
+    expect(CustomerString.inspect).to eq 'CustomerString'
+  end
 
+  it 'new Customer' do 
+    expect(CustomerF.inspect).to eq 'CustomerF'
+  end
+
+  it 'Customer Class' do 
+    expect(CustomerF.class).to eq Class
+  end
+
+  it 'Customer members' do 
+    expect(CustomerF.members).to eq [:name, :address, :zip]
+  end
+
+  it 'joe members' do 
+    expect(joeF.members).to eq [:name, :address, :zip]
+  end
+
+  it 'joe class' do 
+    expect(joeF.class).to eq CustomerF
+  end
+
+  it 'get joe.name' do 
+    expect(joeF.name).to eq 'Joe Smith'
+  end
+
+  it 'joe[:address]' do 
+    expect(joeF[:address]).to eq '123 Maple, Anytown NC'
+  end
+
+  it 'joe[2]' do 
+    expect(joeF[2]).to eq 12345
+  end
+
+  it 'joe.name = ' do 
+    joeF.name = 'Joe Smith jun'
+    expect(joeF.name).to eq 'Joe Smith jun'
+  end
+
+  it 'joe[:address] = ' do 
+    joeF[:address] = '321 Maple, Anytown NC'
+    expect(joeF[:address]).to eq '321 Maple, Anytown NC'
+  end
+
+  it 'joe[2] = ' do 
+    joeF[2] = 54321
+    expect(joeF[2]).to eq 54321
+  end
+
+  it 'joe[CustomerF]' do 
+    expect {
+      joeF[CustomerF]
+    }.to raise_error(TypeError)
+
+  end
+
+  it 'Customer with block' do
+    CustomerFb = Factory.new(:name, :address) do 
+      def greeting
+        "Hellow #{name}!"
+      end
+    end
+    expect(CustomerFb.new("Dave", "123 Main").greeting).to eq "Hellow Dave!"
   end
 
 end
